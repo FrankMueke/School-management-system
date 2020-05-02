9546c68d8571ffb88e12d999f813d9895aa2ae6d
mueke <frankmueke98@gmail.com>
2020-05-02 13:25:18 +0300

    First Commit

---
+++ src/classroom/Course.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classroom;
+
+/**
+ *
+ * @author Mueke
+ */
+public class Course {
+    //Course properties
+    private String courseCode;
+    private String courseName;
+    private Faculty faculty;
+    private int courseYear;
+    
+    //getters and setters
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    public int getCourseYear() {
+        return courseYear;
+    }
+
+    public void setCourseYear(int courseYear) {
+        this.courseYear = courseYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" + "courseCode=" + courseCode + ", courseName=" + courseName + ", faculty=" + faculty + ", courseYear=" + courseYear + '}';
+    }
+    
+    
+    
+}
---
+++ src/classroom/Student.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classroom;
+
+/**
+ *
+ * @author Mueke
+ */
+public class Student extends Person{
+
+    /**
+     * Fields/properties
+     */
+    private Course course;
+    private String phoneNumber;
+    private String username;
+    private String password;
+    //parameterized constructor
+
+    public Student() {
+    }    
+
+    public Student(String name,Course course, String phoneNumber, String username, String password) {
+        this.course = course;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.password = password;
+        this.setName(name);
+    }
+    
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "course=" + course + ", phoneNumber=" + phoneNumber + ", username=" + username + ", password=" + password + '}';
+    }
+
+ 
+    
+}
